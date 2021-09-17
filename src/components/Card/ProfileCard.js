08f1241e132fdcc71545cb6c0d25989828f82e87
Jinok <joseo46@hotmail.com>
2021-09-17 15:08:18 +1000

    add layout

--- src/components/Card/ProfileCard.js
+++ src/components/Card/ProfileCard.js
@@ -16,19 +16,27 @@ function ProfileCard({
 }) {
   return (
     <Col>
-      <Card className='card-container overflow-hidden'>
+      <Card className='card__container overflow-hidden'>
         <Row className='position-relative'>
           <Image src={bg} />
           <Card.ImgOverlay>
             <Image
               src={person}
               roundedCircle
-              className='position-absolute top-100 start-50 translate-middle'
+              className='position-absolute top-100 start-50 translate-middle border border-5 border-white'
             />
           </Card.ImgOverlay>
         </Row>
-
-        <Card.Body></Card.Body>
+        <Card.Body className='card__content'>
+          <Row className='my-5 pt-5'>
+            <p>
+              <span className='fw-bold'>{name} </span>
+              <span className='text-secondary'>{age}</span>
+            </p>
+            <small className='text-secondary'>{city}</small>
+          </Row>
+          <Row></Row>
+        </Card.Body>
       </Card>
     </Col>
   );
--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -3,17 +3,22 @@ import Container from 'react-bootstrap/Container';
 
 function Footer() {
   return (
-    <footer>
+    <footer className='my-3'>
       <Container className='text-center'>
         Challenge by
         <a
           href='https://www.frontendmentor.io?ref=challenge'
           target='_blank'
           rel='noreferrer'
+          className='text-white-50'
         >
           Frontend Mentor
         </a>
-        . Coded by <a href='#top'>Your Name Here</a>.
+        . Coded by{' '}
+        <a href='#top' className='text-white-50'>
+          Jinok
+        </a>
+        .
       </Container>
     </footer>
   );
